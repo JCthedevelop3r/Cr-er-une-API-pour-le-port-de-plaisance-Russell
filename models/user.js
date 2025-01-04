8cb113307cba0fdeeb229f02a54b9923dacef15d
JIICE <minotjeanclaude60@gmail.com>
2025-01-04 09:32:48 +0100

    Creating the user model

---
+++ models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const User = new Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: [true, "Le nom est requis"],
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: [true, "Le nom est requis"],
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
