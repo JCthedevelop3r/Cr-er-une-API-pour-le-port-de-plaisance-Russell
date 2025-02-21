d1158903fd23a9d88ab035ad6cf05d869904b3d9
JIICE <minotjeanclaude60@gmail.com>
2025-02-21 04:48:07 +0100

    Adding images

--- api/public/javascripts/dashboard.style.js
+++ api/public/javascripts/dashboard.style.js
@@ -1,144 +1,152 @@
-/*document
-  .querySelector(".btn-form-create-user")
-  .addEventListener("click", function () {
-    document
-      .getElementById("createUserForm")
-      .scrollIntoView({ behavior: "smooth" });
-});*/
+document.addEventListener("DOMContentLoaded", function () {
+  const btnCreateUser = document.querySelector(".btn-form-create-user");
+  const btnUpdateUser = document.querySelector(".btn-form-update-user");
+  const btnDeleteUser = document.querySelector(".btn-form-delete-user");
+  const btnCreateCatway = document.querySelector(".btn-form-create-catway");
+  const btnUpdateCatwayState = document.querySelector(
+    ".btn-form-update-catway-state"
+  );
+  const btnDeleteCatway = document.querySelector(".btn-form-delete-catway");
+  const btnDisplayCatwayDetails = document.querySelector(
+    ".btn-form-display-catway-details"
+  );
+  const btnSaveReservation = document.querySelector(
+    ".btn-form-save-reservation"
+  );
+  const btnDeleteReservation = document.querySelector(
+    ".btn-form-delete-reservation"
+  );
+  const btnDisplayReservationDetails = document.querySelector(
+    ".btn-form-display-details-reservation"
+  );
 
-const btnCreateUser = document.querySelector(".btn-form-create-user");
-const btnUpdateUser = document.querySelector(".btn-form-update-user");
-const btnDeleteUser = document.querySelector(".btn-form-delete-user");
-const btnCreateCatway = document.querySelector(".btn-form-create-catway");
-const btnUpdateCatwayState = document.querySelector(".btn-form-update-catway-state");
-const btnDeleteCatway = document.querySelector(".btn-form-delete-catway");
-const btnDisplayCatwayDetails = document.querySelector(".btn-form-display-catway-details");
-const btnSaveReservation = document.querySelector(".btn-form-save-reservation");
-const btnDeleteReservation = document.querySelector(".btn-form-delete-reservation");
-const btnDisplayReservationDetails = document.querySelector(".btn-form-display-details-reservation");
+  const createUserForm = document.getElementById("createUserForm");
+  const updateUserForm = document.getElementById("updateUserForm");
+  const deleteUserForm = document.getElementById("deleteUserForm");
+  const createCatwayForm = document.getElementById("createCatwayForm");
+  const updateCatwayStateForm = document.getElementById(
+    "updateCatwayStateForm"
+  );
+  const deleteCatwayForm = document.getElementById("deleteCatwayForm");
+  const displayCatwayDetailsForm = document.getElementById(
+    "displayCatwayDetailsForm"
+  );
+  const saveReservationForm = document.getElementById("saveReservationForm");
+  const deleteReservationForm = document.getElementById(
+    "deleteReservationForm"
+  );
+  const displayReservationDetailsForm = document.getElementById(
+    "displayReservationDetailsForm"
+  );
 
-const createUserForm = document.getElementById("createUserForm");
-const updateUserForm = document.getElementById("updateUserForm");
-const deleteUserForm = document.getElementById("deleteUserForm");
-const createCatwayForm = document.getElementById("createCatwayForm");
-const updateCatwayStateForm = document.getElementById("updateCatwayStateForm");
-const deleteCatwayForm = document.getElementById("deleteCatwayForm");
-const displayCatwayDetailsForm = document.getElementById("displayCatwayDetailsForm");
-const saveReservationForm = document.getElementById("saveReservationForm");
-const deleteReservationForm = document.getElementById("deleteReservationForm");
-const displayReservationDetailsForm = document.getElementById("displayReservationDetailsForm");
-
-
-
-
-
-function ScrollToCreateUserForm() {
-  btnCreateUser.addEventListener("click", function () {
-    createUserForm.scrollIntoView({ behavior: "smooth" });
-    createUserForm.style.backgroundColor = "green";
-    setTimeout(() => {
+  function ScrollToCreateUserForm() {
+    btnCreateUser.addEventListener("click", function () {
+      createUserForm.scrollIntoView({ behavior: "smooth" });
+      createUserForm.style.backgroundColor = "green";
+      setTimeout(() => {
         createUserForm.style.backgroundColor = "";
       }, 2000);
-  });
-}
+    });
+  }
 
-function ScrollToUpdateUserForm() {
-  btnUpdateUser.addEventListener("click", function () {
-  updateUserForm.scrollIntoView({ behavior: "smooth" });
-  updateUserForm.style.backgroundColor = "green";
-  setTimeout(() => {
-      updateUserForm.style.backgroundColor = "";
-    }, 2000);
-  });
-}
+  function ScrollToUpdateUserForm() {
+    btnUpdateUser.addEventListener("click", function () {
+      updateUserForm.scrollIntoView({ behavior: "smooth" });
+      updateUserForm.style.backgroundColor = "green";
+      setTimeout(() => {
+        updateUserForm.style.backgroundColor = "";
+      }, 2000);
+    });
+  }
 
-function ScrollToDeleteUserForm() {
-  btnDeleteUser.addEventListener("click", function () {
-    deleteUserForm.scrollIntoView({ behavior: "smooth" });
-    deleteUserForm.style.backgroundColor = "green";
-    setTimeout(() => {
-      deleteUserForm.style.backgroundColor = "";
-    }, 2000);
-  });
-}
+  function ScrollToDeleteUserForm() {
+    btnDeleteUser.addEventListener("click", function () {
+      deleteUserForm.scrollIntoView({ behavior: "smooth" });
+      deleteUserForm.style.backgroundColor = "green";
+      setTimeout(() => {
+        deleteUserForm.style.backgroundColor = "";
+      }, 2000);
+    });
+  }
 
-function ScrollToCreateCatwayForm() {
+  function ScrollToCreateCatwayForm() {
     btnCreateCatway.addEventListener("click", function () {
       createCatwayForm.scrollIntoView({ behavior: "smooth" });
       createCatwayForm.style.backgroundColor = "green";
       setTimeout(() => {
-      createCatwayForm.style.backgroundColor = "";
+        createCatwayForm.style.backgroundColor = "";
       }, 2000);
     });
-} 
+  }
 
-function ScrollToUpdateCatwayStateForm() {
+  function ScrollToUpdateCatwayStateForm() {
     btnUpdateCatwayState.addEventListener("click", function () {
       updateCatwayStateForm.scrollIntoView({ behavior: "smooth" });
       updateCatwayStateForm.style.backgroundColor = "green";
-    setTimeout(() => {
-      updateCatwayStateForm.style.backgroundColor = "";
-    }, 2000);
+      setTimeout(() => {
+        updateCatwayStateForm.style.backgroundColor = "";
+      }, 2000);
     });
-}
+  }
 
-function ScrollToDeleteCatwayForm() {
+  function ScrollToDeleteCatwayForm() {
     btnDeleteCatway.addEventListener("click", function () {
       deleteCatwayForm.scrollIntoView({ behavior: "smooth" });
       deleteCatwayForm.style.backgroundColor = "green";
-    setTimeout(() => {
-      deleteCatwayForm.style.backgroundColor = "";
-    }, 2000);
+      setTimeout(() => {
+        deleteCatwayForm.style.backgroundColor = "";
+      }, 2000);
     });
-} 
+  }
 
-function ScrollToDisplayCatwayDetailsForm() {
+  function ScrollToDisplayCatwayDetailsForm() {
     btnDisplayCatwayDetails.addEventListener("click", function () {
       displayCatwayDetailsForm.scrollIntoView({ behavior: "smooth" });
       displayCatwayDetailsForm.style.backgroundColor = "green";
-    setTimeout(() => {
+      setTimeout(() => {
         displayCatwayDetailsForm.style.backgroundColor = "";
-    }, 2000);
+      }, 2000);
     });
-} 
+  }
 
-function ScrollToSaveReservationForm() {
+  function ScrollToSaveReservationForm() {
     btnSaveReservation.addEventListener("click", function () {
       saveReservationForm.scrollIntoView({ behavior: "smooth" });
       saveReservationForm.style.backgroundColor = "green";
-    setTimeout(() => {
+      setTimeout(() => {
         saveReservationForm.style.backgroundColor = "";
-    }, 2000);
+      }, 2000);
     });
-} 
+  }
 
-function ScrollToDeleteReservationForm() {
+  function ScrollToDeleteReservationForm() {
     btnDeleteReservation.addEventListener("click", function () {
       deleteReservationForm.scrollIntoView({ behavior: "smooth" });
       deleteReservationForm.style.backgroundColor = "green";
-    setTimeout(() => {
+      setTimeout(() => {
         deleteReservationForm.style.backgroundColor = "";
-    }, 2000);
+      }, 2000);
     });
-} 
+  }
 
-function ScrollToDisplayReservationDetailsForm() {
+  function ScrollToDisplayReservationDetailsForm() {
     btnDisplayReservationDetails.addEventListener("click", function () {
       displayReservationDetailsForm.scrollIntoView({ behavior: "smooth" });
       displayReservationDetailsForm.style.backgroundColor = "green";
-    setTimeout(() => {
+      setTimeout(() => {
         displayReservationDetailsForm.style.backgroundColor = "";
-    }, 2000);
+      }, 2000);
     });
-}
+  }
 
-ScrollToCreateUserForm();
-ScrollToUpdateUserForm();
-ScrollToDeleteUserForm();
-ScrollToCreateCatwayForm();
-ScrollToUpdateCatwayStateForm();
-ScrollToDeleteCatwayForm();
-ScrollToDisplayCatwayDetailsForm();
-ScrollToSaveReservationForm();
-ScrollToDeleteReservationForm();
-ScrollToDisplayReservationDetailsForm();
+  ScrollToCreateUserForm();
+  ScrollToUpdateUserForm();
+  ScrollToDeleteUserForm();
+  ScrollToCreateCatwayForm();
+  ScrollToUpdateCatwayStateForm();
+  ScrollToDeleteCatwayForm();
+  ScrollToDisplayCatwayDetailsForm();
+  ScrollToSaveReservationForm();
+  ScrollToDeleteReservationForm();
+  ScrollToDisplayReservationDetailsForm();
+});
